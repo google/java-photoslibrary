e53ae9b1e8a07846d9f623495ed1155f28fd4668
Minh Bao Truong <tmbao@google.com>
2019-11-19 12:25:01 +1100

    Fix IllegalThreadException when uploading images.
    This is due to starting a thread more than once. Add a lock and
    rerunUpdate boolean to make sure that the update thread results in an
    up-to-date outcome and avoid running multiple update thread at the same
    time.
    
    Change-Id: I556f73b2dd754a496072a4680491c9a8b44e148a

--- sample/src/main/java/com/google/photos/library/sample/views/AbstractCustomView.java
+++ sample/src/main/java/com/google/photos/library/sample/views/AbstractCustomView.java
@@ -3,6 +3,7 @@ package com.google.photos.library.sample.views;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.lang.Thread.State;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
@@ -11,12 +12,17 @@ public abstract class AbstractCustomView extends JFrame {
   private static final String DEFAULT_DIALOG_BACKGROUND = "0xf8f9fa";
 
   private final LoadingView loadingView;
-  private final Thread updateThread;
+
+  private final Thread initializationThread;
+
+  private final Object updateLock = new Object();
+  private Thread updateThread = null;
+  private boolean rerunUpdate = false;
 
   public AbstractCustomView() {
     hideView();
     loadingView = LoadingView.getLoadingView();
-    updateThread = getUpdateThread();
+    initializationThread = getInitializationThread();
 
     getRootPane().setOpaque(false);
     getContentPane().setBackground(Color.decode(DEFAULT_DIALOG_BACKGROUND));
@@ -32,14 +38,24 @@ public abstract class AbstractCustomView extends JFrame {
 
   /** Initializes the view in a new thread and shows loading indicator. */
   public final void initializeView() {
-    loadingView.showView();
-    getInitializationThread().start();
+    if (initializationThread.getState() == State.NEW) {
+      loadingView.showView();
+      initializationThread.start();
+    }
   }
 
   /** Updates the view in a new thread and shows loading indicator. */
   public final void updateView() {
-    loadingView.showView();
-    updateThread.start();
+    synchronized (updateLock) {
+      if ((updateThread == null || updateThread.getState() == State.TERMINATED)
+          && initializationThread.getState() == State.TERMINATED) {
+        updateThread = getUpdateThread();
+        loadingView.showView();
+        updateThread.start();
+      } else {
+        rerunUpdate = true;
+      }
+    }
   }
 
   /** View initialization method */
@@ -49,15 +65,26 @@ public abstract class AbstractCustomView extends JFrame {
   protected abstract void update() throws Exception;
 
   private final Thread getUpdateThread() {
-    return new Thread(
-        () -> {
-          try {
-            update();
+    final Runnable updateRunnable = () -> {
+      try {
+        update();
+      } catch (Exception e) {
+        JOptionPane.showMessageDialog(this, e.getMessage());
+      }
+    };
+    return new Thread(() -> {
+      while (true) {
+        updateRunnable.run();
+        synchronized (updateLock) {
+          if (rerunUpdate) {
+            rerunUpdate = false;
+          } else {
             loadingView.hideView();
-          } catch (Exception e) {
-            JOptionPane.showMessageDialog(this, e.getMessage());
+            break;
           }
-        });
+        }
+      }
+    });
   }
 
   private final Thread getInitializationThread() {
@@ -67,8 +94,16 @@ public abstract class AbstractCustomView extends JFrame {
             moveToCenter();
             initialize();
             requestFocusInWindow();
-            loadingView.hideView();
             showView();
+            synchronized (updateLock) {
+              if (rerunUpdate) {
+                rerunUpdate = false;
+                updateThread = getUpdateThread();
+                updateThread.start();
+              } else {
+                loadingView.hideView();
+              }
+            }
           } catch (Exception e) {
             JOptionPane.showMessageDialog(this, e.getMessage());
           }
